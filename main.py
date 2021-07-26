cd9c33610ff81040933b411c43be27fa9ddefdc0
aphillips04 <2004alexphillips@gmail.com>
2021-07-26 22:00:52 +0100

    add hero stats and items

--- main.py
+++ main.py
@@ -20,16 +20,43 @@ with open("playerdata.json", "r+") as f:
     if not f.read():
         json.dump({}, f, indent=4)
 
+heros = [
+    Hero(None, "Mage", Stats("img/mage.", 1, 2, 3, 5), dict(
+        close = None,
+        range = Item("Bow", "img/bow.", 1),
+        mana = Item("Wand", "img/wand.", 1),
+        defence = Item("Leather", "img/leather.", 1)
+    )),
+    Hero(None, "Paladin", Stats("img/paladin.", 1, 4, 4, 2), dict(
+        close = Item("Short Sword", "img/short_sword.", 1),
+        range = None,
+        mana = Item("Amulet", "img/amulet.", 1),
+        defence = Item("Shield", "img/shield.", 1)
+    )),
+    Hero(None, "Barbarian", Stats("img/barbarian.", 1, 5, 4, 1), dict(
+        close = Item("Sword", "img/sword.", 1),
+        range = None,
+        mana = Item("Amulet", "img/amulet.", 1),
+        defence = Item("Leather", "img/leather.", 1)
+    )),
+    Hero(None, "Rogue", Stats("img/rogue.", 1, 2, 5, 3), dict(
+        close = None,
+        range = Item("Crossbow", "img/crossbow.", 1),
+        mana = Item("Amulet", "img/amulet.", 1),
+        defence = Item("Leather", "img/leather.", 1)
+    ))
+]
+
 monsters = [
-    Monster("Skeleton", [Stats(1, "imgs/skeleton."), Stats(2, "imgs/skeleton."), Stats(3, "imgs/skeleton.")]),
-    Monster("Orc", [Stats(1, "imgs/orc."), Stats(2, "imgs/orc."), Stats(3, "imgs/orc.")]),
-    Monster("Ogre", [Stats(1, "imgs/ogre."), Stats(2, "imgs/ogre."), Stats(3, "imgs/ogre.")]),
-    Monster("Troll", [Stats(1, "imgs/troll."), Stats(2, "imgs/troll."), Stats(3, "imgs/troll.")]),
-    Monster("Wolf", [Stats(1, "imgs/wolf."), Stats(2, "imgs/wolf."), Stats(3, "imgs/wolf.")])
+    Monster("Skeleton", [Stats("imgs/skeleton.", 1, ), Stats("imgs/skeleton.", 2, ), Stats("imgs/skeleton.", 3, )]),
+    Monster("Orc", [Stats("imgs/orc.", 1, ), Stats("imgs/orc.", 2, ), Stats("imgs/orc.", 3, )]),
+    Monster("Ogre", [Stats("imgs/ogre.", 1, ), Stats("imgs/ogre.", 2, ), Stats("imgs/ogre.", 3, )]),
+    Monster("Troll", [Stats("imgs/troll.", 1, ), Stats("imgs/troll.", 2, ), Stats("imgs/troll.", 3, )]),
+    Monster("Wolf", [Stats("imgs/wolf.", 1, ), Stats("imgs/wolf.", 2, ), Stats("imgs/wolf.", 3, )])
 ]
 
 bosses = [
-    Monster("Dragon", [Stats(1, "imgs/dragon."), Stats(2, "imgs/dragon."), Stats(3, "imgs/dragon.")]),
-    Monster("Demon", [Stats(1, "imgs/demon."), Stats(2, "imgs/demon."), Stats(3, "imgs/demon.")]),
-    Monster("Basalisk", [Stats(1, "imgs/basalisk."), Stats(2, "imgs/basalisk."), Stats(3, "imgs/basalisk.")])
+    Monster("Dragon", [Stats("imgs/dragon.", 1, ), Stats("imgs/dragon.", 2, ), Stats("imgs/dragon.", 3, )]),
+    Monster("Demon", [Stats("imgs/demon.", 1, ), Stats("imgs/demon.", 2, ), Stats("imgs/demon.", 3, )]),
+    Monster("Basalisk", [Stats("imgs/basalisk.", 1, ), Stats("imgs/basalisk.", 2, ), Stats("imgs/basalisk.", 3, )])
 ]
